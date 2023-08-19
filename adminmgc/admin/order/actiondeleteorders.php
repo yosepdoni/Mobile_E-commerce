0fceff2164c4ecdd264315af8f7742f5fd2f8e96
yosepdoni <yosepdoni2905@gmail.com>
2023-08-19 20:17:42 +0700

    hapus lagi

--- adminmgc/admin/order/actiondeleteorders.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include '../koneksi.php';
-    if(isset($_POST['checkout'])){
-    // HAPUS SELURUH ISI DALAM TABLE TRANSAKSIVIEW
-    $sql=mysqli_query($conn, "DELETE FROM orderantampung");
-                
-    }
-?>
